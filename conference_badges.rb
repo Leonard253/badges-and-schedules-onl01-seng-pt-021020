6aa5559a7161ecc2af0bca04b0ae6ff555c17c3d
Trey Williams <leonardwilliamsiii20@gmail.com>
2020-02-16 19:06:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,8 @@ end
 
 def batch_badge_creator(attendees)
   new_attendees = [ ]
-  attendees.each do |name|
-  new_attendees.push("Hello my name is #{name}.")
+  attendees.each do |attendee|
+  new_attendees.push("Hello my name is #{attendee}.")
 end
 new_attendees
 end
